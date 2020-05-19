6b450eada444ba22eee48bfefb1327efc3f47aa5
Antoine Ribeiro <antoine.ribeiro37@gmail.com>
2020-05-19 19:46:03 +0200

    fetch api

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,11 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from 'react';
+
 import cx from 'classnames';
 import s from './Projects.module.css';
 
-function Projects() {
+function Projects({ altermap, wineNote, btpn, heko }) {
   const [displayInfo, setDisplayInfo] = useState(false);
 
   const handleInfo = () => {
@@ -12,11 +15,26 @@ function Projects() {
   return (
     <div className={s.projects}>
       <h2>My recent works</h2>
-      <img className={s.img} src="/wine-note-2.png" alt="wine-note-project" onClick={handleInfo} />
-      <p className={cx(displayInfo ? s.info : s.info__hidden)}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium perferendis
-        nesciunt amet repudiandae veniam ullam aspernatur illum rerum minus.
-      </p>
+      <article>
+        <img className={s.img} src={heko.mockup} alt="heko-project" onClick={handleInfo} />
+        <h2>{heko.title}</h2>
+        <p className={cx(displayInfo ? s.info : s.info__hidden)}>{heko.description}</p>
+      </article>
+      <article>
+        <img className={s.img} src={altermap.mockup} alt="altermap-project" onClick={handleInfo} />
+        <h2>{altermap.title}</h2>
+        <p className={cx(displayInfo ? s.info : s.info__hidden)}>{altermap.description}</p>
+      </article>
+      <article>
+        <img className={s.img} src={btpn.mockup} alt="btpn-project" onClick={handleInfo} />
+        <h2>{btpn.title}</h2>
+        <p className={cx(displayInfo ? s.info : s.info__hidden)}>{btpn.description}</p>
+      </article>
+      <article>
+        <img className={s.img} src={wineNote.mockup} alt="Wine-Note-project" onClick={handleInfo} />
+        <h2>{wineNote.title}</h2>
+        <p className={cx(displayInfo ? s.info : s.info__hidden)}>{wineNote.description}</p>
+      </article>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 import styles from './index.module.css';
 import AboutMe from '../components/AboutMe/AboutMe';
 import Projects from '../components/Projects/Projects';
 
-function HomePage() {
+function HomePage({ altermap, wineNote, btpn, heko }) {
   return (
     <div>
       <section className={styles.accueil}>
         <h1>Hi, I'm Antoine. Fullstack developer</h1>
       </section>
       <AboutMe />
-      <Projects />
+      <Projects altermap={altermap} wineNote={wineNote} btpn={btpn} heko={heko} />
       <Link href="/contact">
         <button type="button">Contact Me</button>
       </Link>
@@ -19,4 +20,22 @@ function HomePage() {
   );
 }
 
+export async function getStaticProps() {
+  const hekoRes = await fetch('http://localhost:3001/api/v1/project/1');
+  const altermapRes = await fetch('http://localhost:3001/api/v1/project/2');
+  const btpnRes = await fetch('http://localhost:3001/api/v1/project/3');
+  const wineNoteRes = await fetch('http://localhost:3001/api/v1/project/4');
+  const heko = await hekoRes.json();
+  const altermap = await altermapRes.json();
+  const btpn = await btpnRes.json();
+  const wineNote = await wineNoteRes.json();
+  return {
+    props: {
+      heko,
+      altermap,
+      btpn,
+      wineNote,
+    },
+  };
+}
 export default HomePage;
